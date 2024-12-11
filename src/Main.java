a228108d53598a17c4b5aae7c0168682fbc54bb6
Hemalatha-Chandrasekar <hema.alliswell@gmail.com>
2024-12-11 14:48:12 -0600

    HashSet Commit

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Main {
+    public static void main(String[] args) {
+        exampleOne exampleOne = new exampleOne();
+        exampleOne.HashListDemo();
+        exampleTwo exampleTwo = new exampleTwo();
+        exampleTwo.hastSetDemo2();
+    }
 }
--- src/exampleOne.java
+++ src/exampleOne.java
@@ -1,4 +1,17 @@
-package PACKAGE_NAME;
+import java.util.HashSet;
 
 public class exampleOne {
+    public void HashListDemo() {
+        HashSet<Integer> evenNumber = new HashSet<>();
+    // Using add() method
+        evenNumber.add(2);
+        evenNumber.add(4);
+        evenNumber.add(6);
+        System.out.println("HashSet: " + evenNumber);
+        HashSet<Integer> numbers = new HashSet<>();
+    // Using addAll() method
+        numbers.addAll(evenNumber);
+        numbers.add(5);
+        System.out.println("New HashSet: " + numbers);
+    }
 }
